147514ff709b089348e14d6863511a86a03d566e
adharsh2000 <87477816+adharsh2000@users.noreply.github.com>
2023-08-05 17:12:22 +0530

    Add files via upload

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,16 @@
-
 import firebase from "firebase";
-import 'firebase/auth'
-import 'firebase/firebase'
+import 'firebase/auth';
+import 'firebase/firebase';
+import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1yrP_wm4PypknsFOH_MLcLEZWn7-hQH0",
-    authDomain: "olx-clone-83302.firebaseapp.com",
-    projectId: "olx-clone-83302",
-    storageBucket: "olx-clone-83302.appspot.com",
-    messagingSenderId: "529775863042",
-    appId: "1:529775863042:web:d1b2fe8a351cf9a7418e1b",
-    measurementId: "G-JY01T34DPX"
-  };
+  apiKey: "AIzaSyAEmJioKHHanuE1GM34xgoWAbssSpKwztc",
+  authDomain: "olx-clone-e79ff.firebaseapp.com",
+  projectId: "olx-clone-e79ff",
+  storageBucket: "olx-clone-e79ff.appspot.com",
+  messagingSenderId: "842354725447",
+  appId: "1:842354725447:web:e96363764487811af09762",
+  measurementId: "G-MB561CPT1E"
+};
 
-// Initialize Firebase
-export default firebase.initializeApp(firebaseConfig);
-
+  export default firebase.initializeApp(firebaseConfig)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { FirebaseContext } from './store/FirebaseContext';
-import firebase from './firebase/config' 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <FirebaseContext.Provider value={{firebase}}>
-    <App />
-    </FirebaseContext.Provider>
-  </React.StrictMode>
-);
-
+import {FirebaseContext} from './store/Context';
+import Context from './store/Context';
+import firebase from './firebase/config';
+ReactDOM.render(
+<FirebaseContext.Provider value={{firebase}}>
+ <Context>
+<App />
+</Context>
+</FirebaseContext.Provider>
+,document.getElementById('root'));
